b26994a7ba57b47b8fdf38c24e7b8f6f1e8ef79d
Shuki-L <levy.shuki@gmail.com>
2018-02-09 18:15:32 +0200

    fix path

--- ethminer/start.sh
+++ ethminer/start.sh
@@ -9,5 +9,5 @@ CURDIR=`dirname $0`
 
 sudo -S ./overclock.sh
 #/home/revolux/Desktop/ethminer/ethminer -U -S us-east.ethash-hub.miningpoolhub.com:20535 -O shuki.ziggy1:x
-./ethminer -U -S ${REGION}.ethash-hub.miningpoolhub.com:12020 -O ${NAME}:x --farm-retries 0 -FS exit
+./ethminer/ethminer -U -S ${REGION}.ethash-hub.miningpoolhub.com:12020 -O ${NAME}:x --farm-retries 0 -FS exit
 
